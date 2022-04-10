506e440c2947764c1218e2140efd859b447b40e2
davisoaresbatista882@gmail.com <davisoaresbatista882@gmail.com>
2022-04-10 06:53:36 +0000

    nova feat

--- collect_pro_matches_history.py
+++ collect_pro_matches_history.py
@@ -51,4 +51,24 @@ dbutils.fs.ls(aws_path + "pro_matches.history")
 
 # COMMAND ----------
 
+def get_data(**kwargs):
+    url = "https://api.opendota.com/api/proMatches"
+    params =  "&".join([f"{k}={v}" for k,v in kwargs.items()])
+    if params != "":
+        url += "?"+ params
+    print(params)
+    
+    response = requests.get(url)
+    return response.json()
 
+# COMMAND ----------
+
+get_data(less_than_match_id=6517319316)
+
+# COMMAND ----------
+
+df = spark.read.format("json").load(aws_path + "pro_matches.history")
+
+# COMMAND ----------
+
+df.groupBy
