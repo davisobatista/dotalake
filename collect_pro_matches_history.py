86822d69d5c6fd0192c293744d4b6fa7807c59c4
davisoaresbatista882@gmail.com <davisoaresbatista882@gmail.com>
2022-04-10 03:05:20 +0000

    land data to raw

--- collect_pro_matches_history.py
+++ collect_pro_matches_history.py
@@ -15,11 +15,39 @@ response.json()
 
 # COMMAND ----------
 
+#Criar dataframe a partir do json
 df = spark.createDataFrame(response.json())
 
 # COMMAND ----------
 
+#exibir o dataframe em spark
 df.display()
+
+# COMMAND ----------
+
+aws_path = "/mnt/datalake"
+
+# COMMAND ----------
+
+#listar o q ha na pasta
+dbutils.fs.ls("/mnt/datalake")
+
+# COMMAND ----------
+
+dbutils.fs.mkdirs(aws_path + "/raw")
+
+# COMMAND ----------
+
+aws_path = aws_path + "/raw/"
+
+# COMMAND ----------
+
+df.repartition(1).write.format("json").mode("append").save(aws_path + "pro_matches.history")
+
+# COMMAND ----------
+
+#listar o q ha na pasta
+dbutils.fs.ls(aws_path + "pro_matches.history")
 
 # COMMAND ----------
 
